1f9ad35d0dbbab4256ad01c854beb40df7f9d2fe
Pedro Garcia Glez <garciaglez9210@gmail.com>
2024-04-25 21:49:05 -0700

    pruebas unitarias productService

--- src/app/products/services/product/product.service.spec.ts
+++ src/app/products/services/product/product.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(ProductService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
